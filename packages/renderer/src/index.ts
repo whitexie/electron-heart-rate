c8dc0a911d68074dd6fd2eb026f3af4fa6ae1292
ysansan <hippeauxie@gmail.com>
2024-09-05 10:01:32 +0800

    fix

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,6 +1,7 @@
-import {createApp} from 'vue';
+import { createApp } from 'vue';
 import App from '/@/App.vue';
 import '../assets/base.css';
+import '../assets/iconfont.js';
 import 'uno.css';
 
 createApp(App).mount('#app');
